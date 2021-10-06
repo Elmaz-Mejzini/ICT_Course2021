0216c2b871951a72fa02ab9e31e50b5a86485b82
Elmaz-Mejzini <91621002+Elmaz-Mejzini@users.noreply.github.com>
2021-10-06 16:52:11 +0200

    Add files via upload

---
+++ ora5b/WhileStatement.java
@@ -0,0 +1,11 @@
+package ict.kosovo.growth.basic.ora3.ict.java.growth.basic.ora5b;
+
+public class WhileStatement {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 2) {
+            System.out.println("Welcome to Java!");
+            count++;
+        }
+    }
+}
