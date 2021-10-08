e70a00c1aeab54cd88fb897921b6972c4435129c
Elmaz-Mejzini <91621002+Elmaz-Mejzini@users.noreply.github.com>
2021-10-09 01:41:18 +0200

    Add files via upload

---
+++ ict/java/growth/basic/ClassAndObject/Account.java
@@ -0,0 +1,12 @@
+package ict.kosovo.growth.basic.ora3.ict.java.growth.OOP.CllasAndObject;
+
+public class Account {
+    private String name;
+
+    public  void setName(String name) {
+        this.name = name;
+    }
+    public  String getName() {
+        return name;
+    }
+}
---
+++ ict/java/growth/basic/ClassAndObject/Main.java
@@ -0,0 +1,4 @@
+package ict.kosovo.growth.basic.ora3.ict.java.growth.OOP.CllasAndObject;
+
+public class Main {
+}
